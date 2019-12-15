071e0382cb4e24f930172d4cf722e3c319d8bcdb
Robin Lavallée <robin@guibec.com>
2019-12-15 09:55:32 -0800

    updated webpage

---
+++ src/views/CompanyList.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+ 
+class GameList extends Component {
+    state = {
+        games: [],
+        loading: true,
+      };
+ 
+    componentDidMount(){
+        fetch('http://api.streamstracker.com/top_companies')
+          .then(res => res.json())
+          .then(res => {
+              console.log(res)
+              this.setState({
+                  games: res,
+                  loading: false
+              })
+          })
+    }
+
+  render() {
+    if (this.state.loading) {
+        return <div>Loading...</div>
+      }
+      
+    return (
+      <React.Fragment>
+        {this.state.games.map(game => (
+            <li key={game.id} className="list-group-item list-group-item-primary">
+                {game.name} Viewers: {game.viewers}
+            </li>
+          ))}
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default GameList;
