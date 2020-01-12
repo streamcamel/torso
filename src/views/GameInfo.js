a0daa3fa611be57df2c4e43caf1ed3bb3a23b172
Robin Lavallée <robin@guibec.com>
2020-01-11 20:17:16 -0800

    lots of cool images

--- src/views/GameInfo.js
+++ src/views/GameInfo.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class CompanyInfo extends Component {
+class GameInfo extends Component {
     state = {
         logo_url: "",
         name: "",
         viewers: 0,
-        stock_ticker: ""
       };
  
     componentDidMount(){
+        var image = this.props.logo_url.replace("{width}", "70").replace("{height}", "70");
+
         this.setState({
-            logo_url: this.props.logo_url,
+            logo_url: image,
+            game_id: this.props.game_id,
             name: this.props.name,
-            viewers: this.props.viewers,
-            stock_ticker: this.props.stock_ticker
+            viewers: this.props.viewers
         });
     }
 
@@ -26,19 +27,18 @@ class CompanyInfo extends Component {
             <img src={this.state.logo_url}/>
         </div>
         <div class="ri-name">
-            <Link to={(this.state.name === null) ? "/" : "/company/" + this.state.name}>
+            <Link to={(this.state.name === null) ? "/" : "/game/" + this.state.name}>
                 {this.state.name}
             </Link>
         </div>
         <div class="ri-info">
-            <Link to={(this.state.name === null) ? "/" : "/company/" + this.state.name}>
+            <Link to={(this.state.name === null) ? "/" : "/game/" + this.state.name}>
                 {this.state.viewers}
             </Link>
         </div>
-        {this.state.stock_ticker}
       </>
     )
   }
 }
 
-export default CompanyInfo;
+export default GameInfo;
