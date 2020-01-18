ccb7030aa9eb181969d1a0d6cb77d3db098ed111
Robin Lavallée <robin@guibec.com>
2020-01-17 22:26:11 -0800

    game support long range

--- src/views/CompanyList.js
+++ src/views/CompanyList.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import CompanyInfo from "./CompanyInfo";
-import ListGroup from 'react-bootstrap/ListGroup';
 
 class GameList extends Component {
     state = {
