058be35c1a3a591e9e0ae875dae3efac96a28354
Robin Lavallée <robin@guibec.com>
2020-01-30 21:30:16 -0800

    ads + privacy policy

---
+++ src/views/PrivacyPolicyLink.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PrivacyPolicyLink extends Component {
+    render() {
+        return (<a href="/privacy">Privacy Policy</a>)
+    }
+}
+
+export default PrivacyPolicyLink;
