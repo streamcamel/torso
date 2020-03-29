21fd623a4124563ec521cea0b37a64ea184da1f8
Robin Lavallée <robin@guibec.com>
2020-03-29 16:00:46 -0500

    added images on game

--- src/views/GameHeader.js
+++ src/views/GameHeader.js
@@ -25,7 +25,8 @@ class GameHeader extends Component {
 
     return (
       <>
-        This is the Game Header for { this.state.game.name } - Great Game!
+        <h2> { this.state.game.name }</h2>
+        <img class="cover" src={this.state.game.box_art_url.replace("{width}", "280").replace("{height}", "210")}/>
       </>
     )
   }
