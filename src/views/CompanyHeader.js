e32156b319892be7a6ab00bae51a99f987e5be7f
Robin Lavallée <robin@guibec.com>
2020-04-01 08:55:59 -0500

    add average viewers

--- src/views/CompanyHeader.js
+++ src/views/CompanyHeader.js
@@ -8,16 +8,51 @@ class CompanyHeader extends Component {
       };
  
     componentDidMount(){
-        fetch('https://api.streamcamel.com/companies/' + this.props.company)
-        .then(res => res.json())
-        .then(res => {
+      var companyUrl = 'https://api.streamcamel.com/companies/' + this.props.company;
+      var viewerUrl = 'https://api.streamcamel.com/viewers?before=' + '2020-04-01T01:42:05.947Z&after=2019-04-01T01:42:05.947Z&company=epic-games';
+
+      var fetchData = () => {
+        const urls = [
+          companyUrl,
+          viewerUrl
+        ];
+
+        const allRequests = urls.map(url => 
+          fetch(url).then(response => response.json())
+          );
+
+        return Promise.all(allRequests);
+      };
+
+      fetchData().then(arrayOfResponses => {
+        //console.log("The data we got from the server:", arrayOfResponses);
+
             this.setState({
-                company: res[0],
-                slug: this.props.company,
-                loading: false
-            })
-        })
+                 company: arrayOfResponses[0][0],
+                 viewers: arrayOfResponses[1],
+                 slug: this.props.company,
+                 loading: false
+             });
+            });
     }
+
+  /**
+   * Get the average viewers for the last numDays
+   * If there are less than numDays available, then stop after
+   */
+  averageViewers(numDays) {
+    var sum = 0;
+    var i;
+    for (i = 0; i < Math.min(this.state.viewers.length, numDays); ++i) {
+      sum += this.state.viewers[i].viewers_count;
+    }
+
+    if (i == 0) {
+      return 0;
+    } else {
+      return sum / i;
+    }
+  }
 
   render() {
     if (this.state.loading) {
@@ -28,6 +63,11 @@ class CompanyHeader extends Component {
       <>
         <h2> { this.state.company.name }</h2>
         <img class="cover" src={ "https:" + this.state.company.url.replace("/t_thumb/", "/t_logo_med/")}/>
+        <h3>Last Week</h3>
+        <p>Hours Streamed: </p>
+        <p>Average Viewers: { Math.ceil(this.averageViewers(7)) }</p>
+        <p>Peak Viewers: </p>
+        <p>Hours Watched: </p>
       </>
     )
   }
