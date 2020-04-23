c1eb815cc41459e10dc79e26d79081004ced5b96
pallaire <pallaire@roblox.com>
2020-04-23 13:41:05 -0700

    Adding Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React , { useState, useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 import './App.css';
 
 import Companies from './components/Companies';
 import MainToolbar from './components/MainToolbar';
 import { PageHeader, PageFooter } from './components/StaticCompnents';
+import { useLocation, useHistory, } from "react-router-dom";
 
+import * as appConfig from './config'
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    useLocation,
-    useHistory, 
-} from "react-router-dom";
-  
+ReactGA.initialize(appConfig.trackingId);
+
 
 // Valid Routes
 //    /
@@ -22,10 +20,17 @@ import {
 //    /game/gameSlug
 //    /privacy
 
-
 const App = () => {
     let location = useLocation();
     let history = useHistory();
+
+
+    useEffect(() => {
+        console.log('Sending GA analytics : ' + location.pathname);        
+        ReactGA.set({page: location.pathname});
+        ReactGA.pageview(location.pathname); // Record a pageview for the given page
+
+    }, [location]);
 
     const onGoHome = () => {
         history.push('/');
--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export function backendURL(path) {
     return "https://api.streamcamel.com" + path;
 }
+
+export const trackingId = "UA-85057016-2";
