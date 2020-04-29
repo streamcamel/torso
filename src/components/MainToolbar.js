36f0ece45d6ccf16e04117743532fa3af51ba0c1
pallaire <pallaire@roblox.com>
2020-04-29 08:37:12 -0700

    Fixed the search bar ( Issue #10 )

--- src/components/MainToolbar.js
+++ src/components/MainToolbar.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { useLocation, useHistory, } from "react-router-dom";
+import React, { useRef, useEffect } from 'react';
+import { useLocation, useHistory } from "react-router-dom";
+import * as utils from '../utils'
 
 // class MainToolbar extends React.Component {
 const MainToolbar = () => {
     let history = useHistory();
+    let location = useLocation();
+
+    const refInput = useRef(null);
 
     const onSearch = (event) => {
         if(event.key === 'Enter') {
             event.preventDefault();
-            history.push('/search/' + event.target.value);
-            event.target.value = "";
+            if(event.target.value === ''){
+                history.push('/');
+            } else {
+                history.push('/search/' + event.target.value);
+            }
         }
     }
+
+    useEffect(() => {
+        let command = utils.pathToCommand(location.pathname);
+        if(command !== 'search') {
+            if(refInput.current !== undefined  &&  refInput.current !== null){
+                refInput.current.value = '';
+            }
+        }
+    }, [location]);
 
     return (
         <div className="MainToolbar">
             <div className="MainToolbarSectionSearch">
                 <span className="MainToolbarSectionTitle">Search </span>
-                <input type="text" onKeyDown={onSearch} />
+                <input ref={refInput} type="text" onKeyDown={onSearch} />
             </div>
         </div>
     );
