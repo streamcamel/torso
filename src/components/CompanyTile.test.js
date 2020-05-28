b2700530ec0569aa99a5fc193a3eef3034aebad9
EC2 Default User <ec2-user@ip-172-31-12-236.us-west-1.compute.internal>
2020-05-28 14:47:46 +0000

    Added CompanyTile tests

---
+++ src/components/CompanyTile.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
+import CompanyTile from './CompanyTile';
+
+// Mocking window.getSelection, because it doesn't exist in test env.
+global.getSelection = () => '';
+
+// Mocking data to pass as props
+const data = [{
+    'alpha_channel': 1,
+    'height': 326,
+    'id': 41,
+    'image_id': 'cl2fe',
+    'name': 'Riot Games',
+    'slug': 'riot-games',
+    'stock_ticker': null,
+    'stream_count_average': 0,
+    'stream_count_peak': 0,
+    'url': '//images.igdb.com/igdb/image/upload/t_thumb/cl2fe.jpg',
+    'viewer_count_average': 7654321,
+    'viewer_count_peak': 0,
+    'width': 1024
+}];
+
+
+test('CompanyTile component: Creation', () => {
+    const history = createMemoryHistory()
+
+    let acompany = data[0];
+    let key = 'companykey'+acompany.id
+
+    const { getByText, getByRole} = render(
+        <Router history={history}>
+            <CompanyTile key={key} company={acompany}/>
+        </Router>
+    );
+
+    // Tile Name
+    expect(getByText('Riot Games')).toBeTruthy();
+    
+    // Tile Viewers
+    expect(getByText('7,654,321 viewers')).toBeTruthy();
+    
+    // Tile Image
+    let icon = getByRole('img');
+    expect(icon.src).toEqual('https://images.igdb.com/igdb/image/upload/t_logo_med/cl2fe.png');
+});
+
+
+test('CompanyTile component: Click on tile', () => {
+    const history = createMemoryHistory();
+    history.push('/');
+
+    let acompany = data[0];
+    let key = 'companykey'+acompany.id
+    
+    const { getByAltText } = render(
+        <Router history={history}>
+            <CompanyTile key={key} company={acompany}/>
+        </Router>
+    );
+
+    //Click on the tile
+    userEvent.click(screen.getByRole('img'));
+
+    expect(history.location.pathname).toEqual('/company/riot-games');
+});
