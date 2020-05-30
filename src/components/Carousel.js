cd2803b569abd09a1219589bcf27de6df948a2cf
EC2 Default User <ec2-user@ip-172-31-12-236.us-west-1.compute.internal>
2020-05-30 22:25:27 +0000

    Rename GamePage to SingleGamePage

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 
 import CompaniesListPage from './CompaniesListPage'
-import GamePage from './GamePage'
+import SingleGamePage from './SingleGamePage'
 import MainChart from './MainChart'
 import GamesListPage from './GamesListPage'
 import TopGames from './TopGames'
@@ -23,7 +23,7 @@ const Carousel = () => {
                     <GamesListPage />
                 </Route>
                 <Route path="/game">
-                    <GamePage />
+                    <SingleGamePage />
                 </Route>
                 <Route path="/topgames">
                     <TopGames />
