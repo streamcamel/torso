552558d60833d29c64456364278b4bb413cf5728
Robin Lavallée <robin@guibec.com>
2020-06-15 18:13:16 -0500

    Add intro text

---
+++ src/components/IntroText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IntroText = (props) => {
+    return (
+        <div className="IntroText">
+            <h3>
+                <p>Welcome to StreamCamel</p>
+                <p>Analytics from Twitch on every video game companies</p>
+            </h3>
+        </div>
+    );
+}
+
+export default IntroText;
