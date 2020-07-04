c7949807928bc7197bd06f6f20c0d0a52d98689a
Robin Lavallée <robin@guibec.com>
2020-07-04 13:10:01 -0500

    fix clip size window

--- src/components/ModalClip.js
+++ src/components/ModalClip.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Modal from 'react-modal';
+import { isLocalNetwork } from '../utils';
 
 const ModalClip = (props) => {
 
@@ -13,7 +14,13 @@ const ModalClip = (props) => {
     useEffect(() => {
     }, [isOpen]);
 
-    let embedUrl = 'https://clips.twitch.tv/embed?clip=' + props.clip.id + '&parent=www.streamcamel.com&parent=localhost';
+
+    let embedUrl = 'https://clips.twitch.tv/embed?clip=' + props.clip.id + '&parent=www.streamcamel.com';
+    if (isLocalNetwork()) {
+        embedUrl += '&parent=localhost';
+    }
+    let title = 'Playing Clip ' + props.clip.title;
+
     return (
             <Modal
             className="ModalClipDialog" 
@@ -21,10 +28,10 @@ const ModalClip = (props) => {
             >
                 <div className="ModalClip">
                 <span title="X" className="ModalClipCloseButton" onClick={onHideClip}>X</span>
-                    <iframe title="Clip for TODO"
+                    <iframe title={title}
                             src={embedUrl} 
-                            height="360" 
-                            width="640" 
+                            height="540" 
+                            width="960" 
                             frameBorder="0" 
                             scrolling="no"
                             allowFullScreen={true}>
