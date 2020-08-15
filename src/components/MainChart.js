6b564db43e5c7f923d6474214433f33150cb2b83
Robin Lavallée <robin@guibec.com>
2020-08-15 13:55:20 -0500

    proper tooltips

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -316,8 +316,34 @@ const MainChart = (props) => {
     }
 
     let chartData = convertDataToChartData(data);
+
+    const exampleData = {
+        datasets: [{
+            label: 'Sales',
+            type:'line',
+            data: [51, 65, 40, 49, 60, 37, 40],
+            fill: false,
+            borderColor: '#EC932F',
+            backgroundColor: '#EC932F',
+            pointBorderColor: '#EC932F',
+            pointBackgroundColor: '#EC932F',
+            pointHoverBackgroundColor: '#EC932F',
+            pointHoverBorderColor: '#EC932F',
+            yAxisID: 'y-axis-2'
+          },{
+            type: 'bar',
+            label: 'Visitor',
+            data: [200, 185, 590, 621, 250, 400, 95],
+            fill: false,
+            backgroundColor: '#71B37C',
+            borderColor: '#71B37C',
+            hoverBackgroundColor: '#71B37C',
+            hoverBorderColor: '#71B37C',
+            yAxisID: 'y-axis-1'
+          }]
+    };
+
     const testdata = {
-        labels: chartData[0],
         datasets: [{
             type: 'line',
             label: 'Viewers',
@@ -354,6 +380,55 @@ const MainChart = (props) => {
         ]
     };
 
+    const exampleOptions = {
+        responsive: true,
+        tooltips: {
+          mode: 'label'
+        },
+        elements: {
+          line: {
+            fill: false
+          }
+        },
+        scales: {
+          xAxes: [
+            {
+              display: true,
+              gridLines: {
+                display: false
+              },
+              labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            }
+          ],
+          yAxes: [
+            {
+              type: 'linear',
+              display: true,
+              position: 'left',
+              id: 'y-axis-1',
+              gridLines: {
+                display: false
+              },
+              labels: {
+                show: true
+              }
+            },
+            {
+              type: 'linear',
+              display: true,
+              position: 'right',
+              id: 'y-axis-2',
+              gridLines: {
+                display: false
+              },
+              labels: {
+                show: true
+              }
+            }
+          ]
+        }
+    };
+
     const options = {
         maintainAspectRatio: false,
         responsive: true,
@@ -363,10 +438,9 @@ const MainChart = (props) => {
             fullWidth: true,
             reverse: false,
         },
-        // tooltip:{
-        //     mode: 'nearest',
-        //     intersect: false,
-        // },
+        tooltips:{
+             mode: 'label',
+         },
         // hover:{
         //     mode: 'nearest',
         //     intersect: false,
@@ -379,7 +453,8 @@ const MainChart = (props) => {
                 },
                 time: {
                     unit: timeUnit
-                }
+                },
+                labels: chartData[0]
             }],
             yAxes: [{
                 type: 'linear',
@@ -440,6 +515,7 @@ const MainChart = (props) => {
             <h2 className="ChartTitle">{title}</h2>
 
             <div className="MainChart">
+                {/* <Line ref={refChart} data={exampleData} options={exampleOptions} /> */}
                 <Line ref={refChart} data={testdata} options={options} />
             </div>
 
