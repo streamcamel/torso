7e332521afd4faf7549f4b164447bf8b1bf403a3
Robin Lavallée <robin@guibec.com>
2020-08-23 19:16:22 -0500

    staging/dev

--- src/config.js
+++ src/config.js
@@ -1,5 +1,21 @@
+const hostname = window && window.location && window.location.hostname;
+
+let backendHost;
+if (hostname === 'www.streamcamel.com') {
+    backendHost = 'https://api.streamcamel.com';
+} else if (hostname === 'www.staging.streamcamel.com') {
+    backendHost = 'https://api.staging.streamcamel.com';
+} else if (hostname === 'www.dev.streamcamel.com') {
+    backendHost = 'https://api.dev.streamcamel.com';
+} else {
+    // TODO: Make this works with localhost
+    backendHost = 'https://api.streamcamel.com';
+}
+
 export function backendURL(path) {
-    return "https://api.streamcamel.com" + path;
+
+
+    return backendHost + path;
 }
 
 export const trackingId = "UA-85057016-2";
