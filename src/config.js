2a9a9485e8073943504152ccb125ac77698edaf7
rlavalle <robin@guibec.com>
2020-08-24 18:50:37 -0500

    Staging/Dev API through hostname (#120)

--- src/config.js
+++ src/config.js
@@ -1,5 +1,24 @@
+const hostname = window && window.location && window.location.hostname;
+
+// Replace 'false' with 'true' when testing backend API locally.
+const isLocal = false && window && window.location && 
+                (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
+
+let backendHost;
+if (hostname === 'www.streamcamel.com') {
+    backendHost = 'https://api.streamcamel.com';
+} else if (hostname === 'www.staging.streamcamel.com') {
+    backendHost = 'https://api.staging.streamcamel.com';
+} else if (hostname === 'www.dev.streamcamel.com') {
+    backendHost = 'https://api.dev.streamcamel.com';
+} else if (isLocal) {
+    backendHost = 'http://localhost:8800';
+} else {
+    backendHost = 'https://api.streamcamel.com';
+}
+
 export function backendURL(path) {
-    return "https://api.streamcamel.com" + path;
+    return backendHost + path;
 }
 
 export const trackingId = "UA-85057016-2";
