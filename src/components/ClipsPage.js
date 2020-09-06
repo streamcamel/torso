7575574eb966f1fef606d69df3f1219d24c2cf04
Robin Lavallée <robin@guibec.com>
2020-09-05 21:58:15 -0500

    fix today clips

--- src/components/ClipsPage.js
+++ src/components/ClipsPage.js
@@ -5,7 +5,7 @@ import ClipsCarousel from './ClipsCarousel';
 const ClipsPage = () => {
     return (
         <>
-            <ClipsCarousel className="ClipsCarousel" period="today" title="Today's Top Clips"/>
+            <ClipsCarousel className="ClipsCarousel" period="day" title="Today's Top Clips"/>
             <ClipsCarousel className="ClipsCarousel" period="week" title="This Week Top Clips"/>
             <ClipsCarousel className="ClipsCarousel" period="month" title="This Month Top Clips"/>
             <ClipsCarousel className="ClipsCarousel" period="all" title="All Time Top Clips"/>
