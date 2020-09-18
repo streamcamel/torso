9ef3b02fa071f9e9426650658c9885e998df255c
rlavalle <robin@guibec.com>
2020-09-18 09:53:58 -0500

    Reduce streamers time range to 2 days and limit to 48 entries (#124)

--- src/components/TopStreamersPage.js
+++ src/components/TopStreamersPage.js
@@ -22,7 +22,7 @@ const TopGamesPage = () => {
 
         if(prevPath !== location.pathname)
         {
-            let url = appConfig.backendURL('/users_stats?limit=100&period=1w');
+            let url = appConfig.backendURL('/users_stats?limit=48&period=2d');
             fetch(url)
                 .then(res => res.json())
                 .then(res => setData(res))
