00c8ec4cd703caa93f25d5668a8e381ff940ab2a
Robin Lavallée <robin@guibec.com>
2020-09-24 09:32:24 -0500

    fix test

--- src/components/SingleStreamerPage.test.js
+++ src/components/SingleStreamerPage.test.js
@@ -36,10 +36,204 @@ const dataStats = [{
     "viewers":"381719376"
 }];
 
+const userStreams = {
+	"data": [{
+		"id": 39826846654,
+		"start_time": "2020-09-23T21:30:00Z",
+		"end_time": "2020-09-24T06:40:00Z",
+		"peak_view_count": 21482,
+		"average_view_count": 17377,
+		"game_id": 509658,
+		"type": "live",
+		"language": "en",
+		"game_name": "Just Chatting",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Just%20Chatting-{width}x{height}.jpg",
+		"game_slug": null
+	}, {
+		"id": 39815211422,
+		"start_time": "2020-09-22T22:30:00Z",
+		"end_time": "2020-09-23T07:10:00Z",
+		"peak_view_count": 22078,
+		"average_view_count": 19042,
+		"game_id": 509658,
+		"type": "live",
+		"language": "en",
+		"game_name": "Just Chatting",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Just%20Chatting-{width}x{height}.jpg",
+		"game_slug": null
+	}, {
+		"id": 39802871982,
+		"start_time": "2020-09-21T22:10:00Z",
+		"end_time": "2020-09-22T07:30:00Z",
+		"peak_view_count": 26311,
+		"average_view_count": 21410,
+		"game_id": 509658,
+		"type": "live",
+		"language": "en",
+		"game_name": "Just Chatting",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Just%20Chatting-{width}x{height}.jpg",
+		"game_slug": null
+	}, {
+		"id": 39791322526,
+		"start_time": "2020-09-20T22:00:00Z",
+		"end_time": "2020-09-21T10:20:00Z",
+		"peak_view_count": 27372,
+		"average_view_count": 21181,
+		"game_id": 509658,
+		"type": "live",
+		"language": "en",
+		"game_name": "Just Chatting",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Just%20Chatting-{width}x{height}.jpg",
+		"game_slug": null
+	}, {
+		"id": 39777492494,
+		"start_time": "2020-09-19T22:40:00Z",
+		"end_time": "2020-09-20T10:50:00Z",
+		"peak_view_count": 27486,
+		"average_view_count": 20337,
+		"game_id": 509658,
+		"type": "live",
+		"language": "en",
+		"game_name": "Just Chatting",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Just%20Chatting-{width}x{height}.jpg",
+		"game_slug": null
+	}, {
+		"id": 39758608622,
+		"start_time": "2020-09-18T21:30:00Z",
+		"end_time": "2020-09-19T09:40:00Z",
+		"peak_view_count": 28063,
+		"average_view_count": 17147,
+		"game_id": 263490,
+		"type": "live",
+		"language": "en",
+		"game_name": "Rust",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Rust-{width}x{height}.jpg",
+		"game_slug": "rust"
+	}, {
+		"id": 39739499406,
+		"start_time": "2020-09-17T21:50:00Z",
+		"end_time": "2020-09-18T07:00:00Z",
+		"peak_view_count": 27301,
+		"average_view_count": 18377,
+		"game_id": 263490,
+		"type": "live",
+		"language": "en",
+		"game_name": "Rust",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Rust-{width}x{height}.jpg",
+		"game_slug": "rust"
+	}, {
+		"id": 39722350990,
+		"start_time": "2020-09-16T22:30:00Z",
+		"end_time": "2020-09-17T07:30:00Z",
+		"peak_view_count": 25865,
+		"average_view_count": 17289,
+		"game_id": 498859,
+		"type": "live",
+		"language": "en",
+		"game_name": "Mordhau",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Mordhau-{width}x{height}.jpg",
+		"game_slug": "mordhau"
+	}, {
+		"id": 39704635246,
+		"start_time": "2020-09-15T22:50:00Z",
+		"end_time": "2020-09-16T09:10:00Z",
+		"peak_view_count": 23808,
+		"average_view_count": 14889,
+		"game_id": 383341,
+		"type": "live",
+		"language": "en",
+		"game_name": "Miscreated",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Miscreated-{width}x{height}.jpg",
+		"game_slug": "miscreated"
+	}, {
+		"id": 39671820414,
+		"start_time": "2020-09-13T22:00:00Z",
+		"end_time": "2020-09-14T07:50:00Z",
+		"peak_view_count": 22639,
+		"average_view_count": 18611,
+		"game_id": 383341,
+		"type": "live",
+		"language": "en",
+		"game_name": "Miscreated",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Miscreated-{width}x{height}.jpg",
+		"game_slug": "miscreated"
+	}, {
+		"id": 39659374494,
+		"start_time": "2020-09-12T23:10:00Z",
+		"end_time": "2020-09-13T09:00:00Z",
+		"peak_view_count": 22820,
+		"average_view_count": 19278,
+		"game_id": 383341,
+		"type": "live",
+		"language": "en",
+		"game_name": "Miscreated",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/Miscreated-{width}x{height}.jpg",
+		"game_slug": "miscreated"
+	}, {
+		"id": 39644430942,
+		"start_time": "2020-09-11T22:30:00Z",
+		"end_time": "2020-09-12T08:30:00Z",
+		"peak_view_count": 27327,
+		"average_view_count": 21274,
+		"game_id": 65632,
+		"type": "live",
+		"language": "en",
+		"game_name": "DayZ",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/DayZ-{width}x{height}.jpg",
+		"game_slug": "dayz"
+	}, {
+		"id": 39631988942,
+		"start_time": "2020-09-10T22:10:00Z",
+		"end_time": "2020-09-11T10:40:00Z",
+		"peak_view_count": 27026,
+		"average_view_count": 19079,
+		"game_id": 65632,
+		"type": "live",
+		"language": "en",
+		"game_name": "DayZ",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/DayZ-{width}x{height}.jpg",
+		"game_slug": "dayz"
+	}, {
+		"id": 39619634094,
+		"start_time": "2020-09-09T21:40:00Z",
+		"end_time": "2020-09-10T07:40:00Z",
+		"peak_view_count": 29685,
+		"average_view_count": 22431,
+		"game_id": 65632,
+		"type": "live",
+		"language": "en",
+		"game_name": "DayZ",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/DayZ-{width}x{height}.jpg",
+		"game_slug": "dayz"
+	}, {
+		"id": 39607128526,
+		"start_time": "2020-09-08T21:10:00Z",
+		"end_time": "2020-09-09T06:50:00Z",
+		"peak_view_count": 31376,
+		"average_view_count": 25746,
+		"game_id": 65632,
+		"type": "live",
+		"language": "en",
+		"game_name": "DayZ",
+		"box_art_url": "https://static-cdn.jtvnw.net/ttv-boxart/DayZ-{width}x{height}.jpg",
+		"game_slug": "dayz"
+	}],
+	"pagination": {
+		"cursor": "2020-09-08 21:10:00 +0000 UTC"
+	},
+	"In": {
+		"Cursor": "",
+		"Login": "summit1g",
+		"Limit": 15
+	}
+};
+
 test('SingleStreamerPage component: Creation', async () => {
 
     fetch.mockResponse(async (req) => {
-        if(req.url.match(/\/users\//)) {
+        if(req.url.match(/\/users\/.+\/streams/)) {
+            return JSON.stringify(userStreams);
+        } else if(req.url.match(/\/users\//)) {
             return JSON.stringify(data);
         } else if (req.url.match(/users_stats/)) {
             return JSON.stringify(dataStats);
@@ -57,9 +251,10 @@ test('SingleStreamerPage component: Creation', async () => {
         );
     });
 
-    expect(fetch).toHaveBeenCalledTimes(2);
+    expect(fetch).toHaveBeenCalledTimes(3);
     expect(fetch).toHaveBeenCalledWith('https://api.streamcamel.com/users/summit1g');
     expect(fetch).toHaveBeenCalledWith('https://api.streamcamel.com/users/summit1g/clips?limit=12');
+    expect(fetch).toHaveBeenCalledWith('https://api.streamcamel.com/users/summit1g/streams?limit=15');
          
     // Title Name
     expect(screen.getByText('summit1g')).toBeTruthy();
